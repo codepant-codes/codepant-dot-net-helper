d6791fd03f1b34593fe17ea9bb144029cfe09f74
Pishang Ujeniya <pishangujeniya@outlook.com>
2022-05-22 20:52:04 +0530

    Get Description Enum Extension added

---
+++ src/CodePant.DotNet.Helper/EnumHelper/EnumExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace CodePant.DotNet.Helper.EnumHelper
+{
+    public static class EnumExtension
+    {
+
+        /// <summary>
+        /// Provides Description Attribute Value from Enum
+        ///  <seealso href="https://codereview.stackexchange.com/a/157981">Code Answer From t3chb0t</seealso>
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum value)
+        {
+            return
+                value
+                    .GetType()
+                    .GetMember(value.ToString())
+                    .FirstOrDefault()
+                    ?.GetCustomAttribute<DescriptionAttribute>()
+                    ?.Description
+                ?? value.ToString();
+        }
+    }
+}
